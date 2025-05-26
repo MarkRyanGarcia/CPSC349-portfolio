2a17bf04eb9c9a0c42458dc42f10018f9339c198
MarkRyanGarcia <markrgarcia@csu.fullerton.edu>
2025-05-25 21:06:18 -0700

    Update home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC<HomeProps> = () => {
                         An undergraduate student at <span className="highlight">CSU Fullerton 🐘</span> | CS Major, Math Minor
                     </p>
                     <ul className="Home-list">
-                        <li>📷 <span className="highlight">Marketing Team Lead</span> for ACM at CSUF</li>
+                        <li>📷 <span className="highlight">President</span> of ACM at CSUF</li>
                         <li>📐 <span className="highlight">Supplemental Instructor</span> for Calculus II (because Math is just so fun!)</li>
                         <li>💻 I have experience with: Python, C/C++, C#, JavaScript, Typescript, HTML/CSS, GDScript, Intel x86-64 ISA, SQL, and always learning more...</li>
                         <li>💯 Fun fact: I have a collection of <span className="highlight">60+ Rubik's Cubes</span></li>
